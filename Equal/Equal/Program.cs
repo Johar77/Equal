65d99b4aec5410ba7b6211ab1c1c82424f770d1c
Johar77 <Johar77@163.com>
2017-01-01 23:12:23 +0800

    changes log

---
+++ Equal/Equal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Equal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 5;
+            Console.WriteLine(x == y);
+            object a = 5, b = 5;
+            Console.WriteLine(a == b);
+
+            Console.ReadKey();
+        }
+    }
+}
